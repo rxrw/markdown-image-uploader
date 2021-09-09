c42413062c7c7770ff800caf14146ecc9f43959f
Jens Lee <rxrw@me.com>
2021-09-09 17:49:23 +0800

    mp

--- uploader/abs.go
+++ uploader/abs.go
@@ -22,11 +22,14 @@ func NewClient() Uploader {
 	}
 
 	clientType := os.Getenv("CLIENT_NAME")
+	fmt.Printf("使用%s作为上传客户端", clientType)
 	switch clientType {
 	case "aliyun":
 		client = NewAliyunClient()
 	case "qiniu":
 		client = NewQiniuClient()
+	default:
+		client = NewAliyunClient()
 	}
 
 	err := client.Connect()
